1d48729bc0242eab99dc6a3814bb8759e4c8503d
mahdi <mahdigh621@gmail.com>
2024-12-16 17:54:19 +0330

    first commit

---
+++ note-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+import Navbar from './Component/Navbar'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar/>
+        
+    </>
+  )
+}
+
+export default App
---
+++ note-app/src/Component/Navbar.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+        <h1>my note App</h1>
+    </div>
+  )
+}
